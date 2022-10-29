d01367476d3242c3a4d5a69ac05f66efae8c42ee
gauravkumar0519 <gauravkumar0519@gmail.com>
2022-10-29 21:38:42 +0530

    first commit

---
+++ component.js
@@ -0,0 +1,125 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	   "sap/ui/model/json/JSONModel",
+	   "sap/ui/model/resource/ResourceModel"
+	], function (UIComponent, JSONModel, ResourceModel) {
+	"use strict";
+	return UIComponent.extend("cueCorner.Component", {
+		metadata : {
+			"interfaces": ["sap.ui.core.IAsyncContentCreation"],
+			manifest: "json"
+		},
+		init : function () {
+			// call the init function of the parent
+			UIComponent.prototype.init.apply(this, arguments);
+			
+			 // set data model
+	         var oData = {
+	            tables: [
+                    {
+                        id: 1,
+                        type: "pool",
+                        label: "Pool",
+                        startTime: "",
+                        startTime1: 0,
+                        endTime: "",
+                        duration: "",
+                        isStarted: false,
+                        amount: 0
+                    },
+                    {
+                        id: 2,
+                        type: "french",
+                        label: "French 1",
+                        startTime: "",
+                        startTime1: 0,
+                        endTime: "",
+                        duration: "",
+                        isStarted: false,
+                        amount: 0
+                    },
+                    {
+                        id: 3,
+                        type: "french",
+                        label: "French 2",
+                        startTime: "",
+                        startTime1: 0,
+                        endTime: "",
+                        duration: "",
+                        isStarted: false,
+                        amount: 0
+                    },
+                    {
+                        id: 4,
+                        type: "english",
+                        label: "English 1",
+                        startTime: "",
+                        startTime1: 0,
+                        endTime: "",
+                        duration: "",
+                        isStarted: false,
+                        amount: 0
+                    },
+                    {
+                        id: 5,
+                        type: "miniEnglish",
+                        label: "Mini English",
+                        startTime: "",
+                        startTime1: 0,
+                        endTime: "",
+                        duration: "",
+                        isStarted: false,
+                        amount: 0
+                    },
+                    {
+                        id: 6,
+                        type: "English",
+                        label: "English 2",
+                        startTime: "",
+                        startTime1: 0,
+                        endTime: "",
+                        duration: "",
+                        isStarted: false,
+                        amount: 0
+                    }
+                ],
+                prices: {
+                    pool: {
+                        "M10": 20,
+                        "M20": 30,
+                        "M30": 50,
+                        "M40": 70,
+                        "M50": 90,
+                        "M60": 100, 
+                    },
+                    french: {
+                        "M10": 30,
+                        "M20": 50,
+                        "M30": 70,
+                        "M40": 90,
+                        "M50": 110,
+                        "M60": 130, 
+                    },
+                    miniEnglish: {
+                        "M10": 160,
+                        "M20": 140,
+                        "M30": 110,
+                        "M40": 80,
+                        "M50": 60,
+                        "M60": 40, 
+                    },
+                    english: {
+                        "M10": 200,
+                        "M20": 170,
+                        "M30": 140,
+                        "M40": 100,
+                        "M50": 70,
+                        "M60": 50, 
+                    }
+                }
+	         };
+	         var oModel = new JSONModel(oData);
+	         this.setModel(oModel);	        
+		}
+	});
+});
---
+++ controller/App.controller.js
@@ -0,0 +1,89 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+	], function (Controller, MessageToast, JSONModel) {
+	"use strict";
+	return Controller.extend("cueCorner.controller.App", {
+//		onInit : function () {
+//			// set data model on view
+//			var oData = {
+//					recipient : {
+//						name : "World"
+//					}
+//			};
+//			var oModel = new JSONModel(oData);
+//			this.getView().setModel(oModel);
+//		},
+        onTimeStart : function (oEvent) {
+            var oController = this;
+            var oModel = oController.getView().getModel();
+			var sPath = oEvent.getSource().getParent().getBindingContext().getPath();    
+            var data = oModel.getProperty(sPath);
+            var d = new Date();
+            var nhour = d.getHours(),
+            nmin = d.getMinutes(),
+            nsec = d.getSeconds();
+            var AmOrPm = nhour >= 12 ? 'PM' : 'AM';
+            nhour = (nhour % 12) || 12;
+            data.startTime = nhour + ":" + nmin + ":" + nsec + " " + AmOrPm;
+            data.startTime1 = d.getTime();
+            data.endTime = "";
+            data.isStarted = true;
+            oModel.setProperty(sPath, data);                    
+		},
+        onTimeEnd : function (oEvent) {
+            var oController = this;
+            var oModel = oController.getView().getModel();
+			var sPath = oEvent.getSource().getParent().getBindingContext().getPath();    
+            var data = oModel.getProperty(sPath);
+            var d = new Date();
+            var nhour = d.getHours(),
+            nmin = d.getMinutes(),
+            nsec = d.getSeconds();
+            var AmOrPm = nhour >= 12 ? 'PM' : 'AM';
+            nhour = (nhour % 12) || 12;
+            if(nmin < 10){
+                nmin = "0" + nmin;
+            }
+            if(nsec < 10){
+                nsec = "0" + nsec;
+            }
+            data.endTime = nhour + ":" + nmin + ":" + nsec + " " + AmOrPm;
+            var diffMs = d.getTime() -  data.startTime1; 
+            var diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000);
+            data.startTime1 = 0;
+            data.isStarted = false;
+            data.duration = diffMins + " min";
+            data.amount = oController.calculateAmount(data.type ,diffMins);
+            oModel.setProperty(sPath, data);                    
+		},
+        calculateAmount : function(type, minutes){
+            var hours = 0, remainingMinutes = 0, amount = 0;
+            var oController = this;
+            var oModel = oController.getView().getModel();
+            var rate = oModel.getProperty("/prices/" + type);
+            if(minutes > 60){
+                hours =  Math.floor(minutes/60);
+                remainingMinutes = minutes%60;
+            }else{
+                remainingMinutes = minutes;
+            }
+            amount = hours * rate.M60;
+            if(remainingMinutes > 55){
+                amount = amount + rate.M60;
+            }else if(remainingMinutes <= 55 && remainingMinutes > 45){
+                amount = amount + rate.M50;
+            }else if(remainingMinutes <= 45 && remainingMinutes > 35){
+                amount = amount + rate.M40;
+            }else if(remainingMinutes <= 35 && remainingMinutes > 25){
+                amount = amount + rate.M30;
+            }else if(remainingMinutes <= 25 && remainingMinutes > 15){
+                amount = amount + rate.M20;
+            }else if(remainingMinutes <= 15 && remainingMinutes > 5){
+                amount = amount + rate.M10;
+            }                                             
+            return amount;
+        }
+	});
+});
